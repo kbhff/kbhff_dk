fad42d3f28beb9f2813269fb2e1adb7a251523c3
kaestel <martin@think.dk>
2018-04-21 02:16:12 +0200

    Upgrade CI

--- theme/www/janitor/restructure.php
+++ theme/www/janitor/restructure.php
@@ -37,35 +37,75 @@ if(is_array($action) && count($action)) {
 
 		$query = new Query();
 
+		// Code Igniter upgrades
+
+		// (2.0.3)
+		// CREATE INDEX last_activity_idx ON ci_sessions(last_activity); 
+		$UC->addKey(SITE_DB.".ff_sessions", "last_activity", "last_activity_idx");
+
+		// ALTER TABLE ci_sessions MODIFY user_agent VARCHAR(120);
+		$UC->modifyColumn(SITE_DB.".ff_sessions", "user_agent", "VARCHAR(120)");
+
+		// (2.1.1)
+		// ALTER TABLE ci_sessions CHANGE ip_address ip_address varchar(45) default '0' NOT NULL
+		$UC->modifyColumn(SITE_DB.".ff_sessions", "ip_address", "VARCHAR(45) DEFAULT '0' NOT NULL");
+
+
+
+
+
 		// Get Members data from ff_persons
 		// privacy = newsletter
 		// adr2 = streetname
 		// uid = member id
-		$sql = "SELECT firstname, middlename, lastname, adr2, streetno, floor, door, zip, city, tel, email, privacy, last_login, created, changed, uid FROM kbhff1.ff_persons";
+		$sql = "SELECT firstname, middlename, lastname, adr2, streetno, floor, door, zip, city, tel, email, privacy, last_login, created, changed, uid FROM kbhff_dk.ff_persons";
 		$query->sql($sql);
 
+		print_r($query->results());
 		// INSERT DATA IN JANITOR STRUCTURE
 		// NAME DATA -> users
-		// ADDRESS DATA -> user_addresses
+		// ADDRESS DATA -> user_addresses - SKIP
+
 		// EMAIL + TEL -> user_usernames
+
 		// PRIVACY -> user_maillists
 		// UID -> user_members
+
 
 		// DO NOT INSERT PASSWORDS
 
 		// DELETE COLUMNS WITH OLD/MOVED DATA
 
 
-		// DELETE UNUSED TABLES
-		// ff_zipcodes
-		// ff_jobs
-		// ff_chores
-		// ff_xfer
-		// fornavne
-		// piger
-		// unisex
-		// drenge
 
+		// DELETE UNUSED TABLES
+
+		// ff_zipcodes
+		$UC->dropTable(SITE_DB.".ff_zipcodes");
+
+		// ff_jobs
+		$UC->dropTable(SITE_DB.".ff_jobs");
+
+		// ff_chores
+		$UC->dropTable(SITE_DB.".ff_chores");
+
+		// ff_xfer
+		$UC->dropTable(SITE_DB.".ff_xfer");
+
+		// fornavne
+		$UC->dropTable(SITE_DB.".fornavne");
+
+		// piger
+		$UC->dropTable(SITE_DB.".piger");
+
+		// unisex
+		$UC->dropTable(SITE_DB.".unisex");
+
+		// drenge
+		$UC->dropTable(SITE_DB.".drenge");
+
+
+		exit();
 
 	}
 
