59dfc10deae9f8c736a27fb462247c7b3ab85b02
Søren Lyhne <33652234+sorenlyhne@users.noreply.github.com>
2018-05-15 16:34:04 +0200

    Add license HowTos to classes and www.header.php

--- theme/classes/system/page.class.php
+++ theme/classes/system/page.class.php
@@ -1,4 +1,35 @@
 <?php
+/* 
+Foodnet platform
+Copyright (C) 2018  Københavns Fødevarefællesskab and think.dk
+
+Københavns Fødevarefællesskab
+KPH-Projects
+Enghavevej 80 C, 3. sal
+2450 København SV
+Denmark
+mail: bestyrelse@kbhff.dk
+
+think.dk
+Æbeløgade 4
+2100 København Ø
+Denmark
+mail: start@think.dk
+	
+This source code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This source code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this source code.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
 * This file contains the site custom backbone, the Page Class.
 * This class basically only exists to make it easy to add custom page functionality or overwrite behaviours.
--- theme/templates/www.header.php
+++ theme/templates/www.header.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
 <html lang="<?= $this->language() ?>">
 <head>
-	<!-- (c) & (p) KBHFF + think.dk 2018 -->
-	<!-- For detailed copyright license, see /terms -->
+	<!-- 
+	Foodnet platform
+    Copyright (C) 2018  Københavns Fødevarefællesskab and think.dk
+    
+    Københavns Fødevarefællesskab
+    KPH-Projects
+    Enghavevej 80 C, 3. sal
+    2450 København SV
+    Denmark
+    mail: bestyrelse@kbhff.dk
+    
+    think.dk
+    Æbeløgade 4
+    2100 København Ø
+    Denmark
+    mail: start@think.dk
+     
+    This source code is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This source code is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+	along with this source code.  If not, see <http://www.gnu.org/licenses/>. -->
+	
 	<!-- If you want to use or contribute to this code, visit http://parentnode.dk -->
+
+	
 	<title><?= $this->pageTitle() ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="keywords" content="Økologi grøntsager lokal sæson mad fødevarer fællesskab" />
