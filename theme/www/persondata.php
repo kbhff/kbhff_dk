d8ff2a344a7a63549dc5f3d7ecd2850765f8279c
ElisabethSH <e.haabegaard@gmail.com>
2018-06-21 17:02:48 +0200

    create controller and template for Persondata

---
+++ theme/www/persondata.php
@@ -0,0 +1,25 @@
+<?php
+$access_item = false;
+if(isset($read_access) && $read_access) {
+	return;
+}
+
+include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
+
+
+$action = $page->actions();
+
+
+$page->bodyClass("persondata");
+$page->pageTitle("Persondata");
+
+
+
+$page->page(array(
+	"templates" => "pages/personal_data.php"
+	)
+);
+exit();
+
+
+?>
