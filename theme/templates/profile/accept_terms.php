39d9c66f17a39a73372e5129dbdba18de2daaa07
Unknown <ajl@teliamail.dk>
2018-08-24 16:29:22 +0200

    Auto stash before merge of "master" and "origin/master"

--- theme/templates/profile/accept_terms.php
+++ theme/templates/profile/accept_terms.php
@@ -4,7 +4,7 @@ global $UC;
 ?>
 <div class="scene accept_terms i:accept_terms">
 
-	<h1>Accepter vilkår for KBHFF's opbevaring og håndtering af persondata</h1>
+	<h1>Samtykkeerklæring om persondata</h1>
 
 	<p>I forbindelse med overgangen til det ny system, vil vi en enkelt gang bede dig om
 	 dit samtykke til at vi opbevarer og behandler den persondata, du har givet os.
@@ -12,8 +12,8 @@ global $UC;
 
 	<p>Vi opbevarer følgende informationer: Navn, emailadresse, telefonnummer og dine indkøb.</P>
 
-	<p>Afviser du dette samtykke, vil det medføre ophør af dit medlemskab og
-		den persondata, vi har om dig, slettes fra vores server i overensstemmelse med vore retningslinjer.</p>
+	<p>Afviser du dette samtykke, vil det medføre ophør af dit medlemskab, og
+		persondata, vi har om dig, slettes fra vores server i overensstemmelse med vore retningslinjer.</p>
 
 	<p>Læs mere i vore <a href="/persondata">retningslinjer for behandling og opbevaring af persondata</a></p>
 
