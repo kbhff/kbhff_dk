1fc641b362c66a251941cbad0f50e9ab9ff0e2d1
ElisabethSH <e.haabegaard@gmail.com>
2018-11-14 13:38:13 +0100

    Squashed 'theme/www/assets/kbhff-skin/' content from commit 40e6a68
    git-subtree-dir: theme/www/assets/kbhff-skin
    git-subtree-split: 40e6a68c31297877471d136a14cdf184b32618ed

---
+++ js/lib/desktop/i-front.js
@@ -0,0 +1,19 @@
+Util.Objects["front"] = new function() {
+	this.init = function(scene) {
+
+		scene.resized = function() {
+			// u.bug("scene.resized:", this);
+		}
+
+		scene.scrolled = function() {
+			// u.bug("scene.scrolled:", this);
+		}
+
+		scene.ready = function() {
+			// u.bug("scene.ready:", this);
+		}
+
+		// scene is ready
+		scene.ready();
+	}
+}
