a004419ef172aeffcd50744e5535e68ba733ead7
ElisabethSH <e.haabegaard@gmail.com>
2018-11-14 14:40:05 +0100

    Update skin

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,21 +1,19 @@
-document.write('<script type="text/javascript" src="http://udvikler-kbhff.local/js/manipulator/v0_9_2/merged/seg_desktop.js"></script>');
+document.write('<script type="text/javascript" src="assets/kbhff-skin/js/seg_desktop.js"></script>');
 
-document.write('<script type="text/javascript" src="http://udvikler-kbhff.local/js/lib/desktop/u-basics.js"></script>');
+document.write('<script type="text/javascript" src="js/manipulator/v0_9_2/src/beta-u-overlay.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2/src/beta-u-overlay.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-page.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-accept_terms.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-confirm_account.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-create_password.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-update_department.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-update_user_information.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-update_user_password.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-delete_user_information.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-login.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-accept_terms.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-confirm_account.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-create_password.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-update_department.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-update_user_information.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-update_user_password.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-delete_user_information.js"></script>');
 
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-profile.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-profile.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-forgot.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-forgot.js"></script>');
