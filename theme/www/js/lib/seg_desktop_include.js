7717395c52d3ce03dae16dc3e13ebe255d72b8fa
threepwoodd <scope.dk@gmail.com>
2018-12-18 15:14:47 +0100

    Add u-paymentcards include

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,6 +1,7 @@
 document.write('<script type="text/javascript" src="/assets/kbhff-skin/js/seg_desktop.js"></script>');
 
 document.write('<script type="text/javascript" src="/assets/kbhff-skin/js/manipulator/v0_9_2/src/beta-u-overlay.js"></script>');
+document.write('<script type="text/javascript" src="/assets/kbhff-skin/js/manipulator/v0_9_2/src/beta-u-paymentcards.js"></script>');
 
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
