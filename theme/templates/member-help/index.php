8dada3776a730d270a1e94889e87508aa75d4f8a
Søren Lyhne <soren@parentnode.dk>
2018-12-19 17:11:50 +0100

    Update index.php

--- theme/templates/member-help/index.php
+++ theme/templates/member-help/index.php
@@ -1,7 +1,5 @@
 <div class="scene member_help i:scene" itemscope itemtype="http://schema.org/NewsArticle">
 	<h1>Member help</h1>
-	Member help dummy. Member help dummy. Member help dummy. Member help dummy. Member help dummy. Member help dummy. 
-
 	<div>
 		<a href="/medlemshjaelp/tilmelding">Tilmeld nyt medlem</a>
 	</div>
