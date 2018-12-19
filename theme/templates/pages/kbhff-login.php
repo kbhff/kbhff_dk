71064fb8f25f0060e419e6b146b8f22361cad50e
threepwoodd <scope.dk@gmail.com>
2018-12-19 15:55:02 +0100

    Update login page

--- theme/templates/pages/kbhff-login.php
+++ theme/templates/pages/kbhff-login.php
@@ -2,6 +2,11 @@
 global $action;
 global $model;
 
+$IC = new Items();
+$page_item = $IC->getItem(array("tags" => "page:login", "extend" => array("user" => true, "tags" => true, "mediae" => true)));
+if($page_item) {
+	$this->sharingMetaData($page_item);
+}
 
 $forward_url = getVar("forward_url");
 if($forward_url) {
@@ -12,15 +17,52 @@ if($forward_url) {
 $username = stringOr(getPost("username"), session()->value("temp-username"));
 session()->reset("temp-username");
 ?>
+
+
 <div class="scene login i:login">
+
+<? if($page_item && $page_item["status"]):
+	$media = $IC->sliceMedia($page_item); ?>
+	<div class="article i:article id:<?= $page_item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
+
+		<? if($media): ?>
+		<div class="image item_id:<?= $page_item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+		<? endif; ?>
+
+
+		<?= $HTML->articleTags($page_item, [
+			"context" => false
+		]) ?>
+
+
+		<h1 itemprop="headline"><?= $page_item["name"] ?></h1>
+
+		<? if($page_item["subheader"]): ?>
+		<h2 itemprop="alternativeHeadline"><?= $page_item["subheader"] ?></h2>
+		<? endif; ?>
+
+
+		<?= $HTML->articleInfo($page_item, "/bliv-medlem", [
+			"media" => $media,
+		]) ?>
+
+
+		<? if($page_item["html"]): ?>
+		<div class="articlebody" itemprop="articleBody">
+			<?= $page_item["html"] ?>
+		</div>
+		<? endif; ?>
+	</div>
+<? else:?>
 	<h1>Log ind</h1>
 	<p>
 		I medlemssystemet kan du bestille varer, booke vagter og administrere dit medlemsskab.
 		Du kan bruge e-mailadresse, telefonnummer eller medlemsnummer som brugernavn til at logge ind.
 		<strong>Velkommen indenfor!</strong>
 	</p>
+<? endif; ?>
 
-	<?= $model->formStart("dual", array("class" => "login")) ?>
+	<?= $model->formStart("dual", array("class" => "login labelstyle:inject")) ?>
 
 		<? if(message()->hasMessages()): ?>
 		<div class="messages">
@@ -47,16 +89,18 @@ session()->reset("temp-username");
 			<?= $model->input("password", array(
 				"required" => true,
 				"min" => 1,
-				"hint_message" => "Skriv dit password (8-20 tegn)",
-				"error_message" => "Ugyldigt password",
+				"label" => "Adgangskode",
+				"hint_message" => "Skriv din adgangskode (8-20 tegn)",
+				"error_message" => "Ugyldig adgangskode",
 			)); ?>
 		</fieldset>
 
 		<ul class="actions">
 			<?= $model->submit("Log ind", array("class" => "primary", "wrapper" => "li.login")) ?>
-			<li class="forgot">Har du brug for <a href="/login/glemt">hjælp til at logge ind</a>?</li>
 		</ul>
 	<?= $model->formEnd() ?>
+	<p class="forgot">Har du <a href="/login/glemt">glemt din adgangskode</a>?</p>
+
 
 <?	if(defined("SITE_SIGNUP") && SITE_SIGNUP): ?>
 	<div class="signup">
