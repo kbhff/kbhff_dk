e689d16b496357c18542d934d20b4c7336dc2f55
sorenlyhne <lyhnesoren@gmail.com>
2018-12-19 17:12:31 +0100

    Squashed 'theme/www/assets/kbhff-skin/' changes from 1ebe138..351b6a1
    351b6a1 Update banner js
    87142c5 import i-banner.js
    bb0e2ae build
    37d85d7 Update form css
    a2ad0f6 Header spacing
    e650331 Fix article scoping
    
    git-subtree-dir: theme/www/assets/kbhff-skin
    git-subtree-split: 351b6a1dbd899f49eee3ec74371ffaa45d60abab

--- js/lib/seg_desktop_include.js
+++ js/lib/seg_desktop_include.js
@@ -4,5 +4,7 @@ document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"><
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-scene.js"></script>');
 
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-banner.js"></script>');
+
 document.write('<script type="text/javascript" src="/js/lib/desktop/u-basics.js"></script>');
 
