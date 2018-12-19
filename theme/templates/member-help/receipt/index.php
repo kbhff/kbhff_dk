087a78b3c3a3a4f259ea4401ca02d396e22377b4
Søren Lyhne <soren@parentnode.dk>
2018-12-19 21:46:27 +0100

    make receipts into articles

--- theme/templates/member-help/receipt/index.php
+++ theme/templates/member-help/receipt/index.php
@@ -2,16 +2,18 @@
 
 ?>
 <div class="scene member_help_signup_receipt i:scene">
+	<div class="article">
 	<h1>Opret nyt medlem</h1>
-	<h2>Medlemskab er oprettet</h2>
-	<div class="c-wrapper">
-		<div class="c-one-half">
+		<div class="articlebody">
+			<h2>Medlemskab er oprettet</h2>
+		
 			<p>Medlemmet er nu oprettet. Før medlemmet selv kan logge ind på medlemssystemet, skal vedkommende dog finde bekræftelsesmailen, som er sendt til den angivne e-mailadresse og klikke på linket heri.</p>
 			<p>Som kassemester kan du dog allerede nu hjælpe medlemmet med at bestille den første pose ved at klikke nedenfor, eller du kan hjælpe medlemmet med at skrive sig op til den første vagt.</p>
 			<ul class="actions">
 				<li><a class="button clickable" href="#">Gå til medlemsside</a></li>
 				<li><a class="button primary clickable" href="#">Bestil pose til medlem</a></li>
 			</ul>
+
 		</div>
 	</div>
 
--- theme/templates/member-help/receipt/skipped.php
+++ theme/templates/member-help/receipt/skipped.php
@@ -2,10 +2,10 @@
 
 ?>
 <div class="scene member_help_signup_receipt i:scene">
-	<h1>Opret nyt medlem</h1>
-	<h2>Medlemskab er oprettet</h2>
-	<div class="c-wrapper">
-		<div class="c-one-half">
+	<div class="article">
+		<h1>Opret nyt medlem</h1>
+		<div class="articlebody">
+			<h2>Medlemskab er oprettet</h2>
 			<p>Medlemmet er nu oprettet – <span class="highlight">men det er ikke betalt</span>. Før medlemmet selv kan logge ind på medlemssystemet, skal vedkommende dog finde bekræftelsesmailen, som er sendt til den angivne e-mailadresse og klikke på linket heri.</p>
 			<p>Som kassemester kan du dog allerede nu hjælpe medlemmet med at bestille den første pose ved at klikke nedenfor, eller du kan hjælpe medlemmet med at skrive sig op til den første vagt.</p>
 			<ul class="actions">
