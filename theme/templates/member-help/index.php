0db345be4caab285e412d28f661a36eb89736cd8
Søren Lyhne <soren@parentnode.dk>
2018-12-19 18:41:45 +0100

    Update index.php

--- theme/templates/member-help/index.php
+++ theme/templates/member-help/index.php
@@ -1,5 +1,5 @@
 <div class="scene member_help i:scene" itemscope itemtype="http://schema.org/NewsArticle">
-	<h1>Member help</h1>
+	<h1>Medlemshjælp</h1>
 	<div>
 		<a href="/medlemshjaelp/tilmelding">Tilmeld nyt medlem</a>
 	</div>
