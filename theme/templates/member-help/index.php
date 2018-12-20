f49c2289effea22ef1e951da7254bc5020ab1f35
Søren Lyhne <soren@parentnode.dk>
2018-12-20 14:11:01 +0100

    Update index.php

--- theme/templates/member-help/index.php
+++ theme/templates/member-help/index.php
@@ -1,7 +1,13 @@
 <div class="scene member_help i:scene" itemscope itemtype="http://schema.org/NewsArticle">
+	
 	<h1>Medlemshjælp</h1>
-	<div>
-		<a href="/medlemshjaelp/tilmelding">Tilmeld nyt medlem</a>
-	</div>
+
+			<div class="c-wrapper">
+				<div class="c-one-half">
+					<ul class="actions">
+						<li><a class="button primary clickable" href="/medlemshjaelp/tilmelding">Tilmeld nyt medlem</a></li>
+					</ul>
+				</div>
+			</div>
 
 </div>
