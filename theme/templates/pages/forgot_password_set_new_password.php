f0e739fc9ee8b59b099631019e19f54c38c4ba92
threepwoodd <scope.dk@gmail.com>
2019-01-14 10:45:15 +0100

    Update template comments

--- theme/templates/pages/forgot_password_set_new_password.php
+++ theme/templates/pages/forgot_password_set_new_password.php
@@ -1,36 +1,35 @@
 <?php
+// TODO: Explain where we get $action and $model from
 global $action;
 global $model;
 
-
+// Get the saved reset-token in current browser session
 $reset_token = session()->value("temp-reset-token");
 
 ?>
 <div class="scene i:scene defaultEdit userEdit profileEdit">
 	<h1>Reset password</h1>
 
-<? // if($model->checkResetToken($reset_token)): ?>
-
 	<div class="password">
 		<h2>Password</h2>
 
+		<!-- Form -->
 		<?= $model->formStart("resetPassword", array("class" => "reset_password")) ?>
+			<!-- Hidden field with the reset-token we use to validate -->
 			<?= $model->input("reset-token", array("type" => "hidden", "value" => $reset_token)) ?>
 
+			<!-- Input fields -->
 			<fieldset>
 				<?= $model->input("new_password", array("required" => true, "label" => "Ny adgangskode", "hint_message" => "Skriv dit nye adgangskode – 8-20 tegn", "error_message" => "Ugyldig adgangskode")) ?>
 				<?= $model->input("confirm_password", array("required" => true, "label" => "Gentag ny adgangskode", "hint_message" => "Skriv dit nye password igen for at bekræfte", "error_message" => "Dine adgangskoder stemmer ikke overens.")) ?>
 			</fieldset>
+
+			<!-- Buttons -->
 			<ul class="actions">
 				<?= $model->submit("Bekræft ny adgangskode", array("class" => "primary", "wrapper" => "li.save")) ?>
 			</ul>
 		<?= $model->formEnd() ?>
+
 	</div>
-
-<? /* else: ?>
-
-	<p>Your request is invalid. Resetting your password must be completed within 15 minutes.</p>
-
-<? endif; */ ?>
 
 </div>
