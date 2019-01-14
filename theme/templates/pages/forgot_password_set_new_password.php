30466da7913f0ea5f6a43783ae7444bcab9788b1
threepwoodd <scope.dk@gmail.com>
2019-01-14 10:54:55 +0100

    Remove HTML comments

--- theme/templates/pages/forgot_password_set_new_password.php
+++ theme/templates/pages/forgot_password_set_new_password.php
@@ -13,18 +13,14 @@ $reset_token = session()->value("temp-reset-token");
 	<div class="password">
 		<h2>Password</h2>
 
-		<!-- Form -->
 		<?= $model->formStart("resetPassword", array("class" => "reset_password")) ?>
-			<!-- Hidden field with the reset-token we use to validate -->
 			<?= $model->input("reset-token", array("type" => "hidden", "value" => $reset_token)) ?>
 
-			<!-- Input fields -->
 			<fieldset>
 				<?= $model->input("new_password", array("required" => true, "label" => "Ny adgangskode", "hint_message" => "Skriv dit nye adgangskode – 8-20 tegn", "error_message" => "Ugyldig adgangskode")) ?>
 				<?= $model->input("confirm_password", array("required" => true, "label" => "Gentag ny adgangskode", "hint_message" => "Skriv dit nye password igen for at bekræfte", "error_message" => "Dine adgangskoder stemmer ikke overens.")) ?>
 			</fieldset>
 
-			<!-- Buttons -->
 			<ul class="actions">
 				<?= $model->submit("Bekræft ny adgangskode", array("class" => "primary", "wrapper" => "li.save")) ?>
 			</ul>
