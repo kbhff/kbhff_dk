81c3d6eec5fcbe91eeae9a983d6fecb18832ba52
ElisabethSH <e.haabegaard@gmail.com>
2019-01-16 14:15:29 +0100

    Update comments to html.class

--- theme/classes/system/html.class.php
+++ theme/classes/system/html.class.php
@@ -5,9 +5,11 @@
 class HTML extends HTMLCore {
 
 /** 
-* The function takes item and add_path as parameters, and creates a div with the specific item-id as well as meta-data consisting of the validated action path and the crsf-token. 
-* It checks if item has comments. If so, it loops through the comments and prints them together with their corresponding information (published-at and author).
-* Note: Is not yet used in the project. 
+ * Generate a contaginated string with metadata, comments and information. 
+ * 
+ * @param String $item 
+ * @param String $add_path
+ * @return String 
 */
 	function frontendComments($item, $add_path) {
 		global $page;
@@ -19,8 +21,10 @@ class HTML extends HTMLCore {
 		$_ .= '	data-csrf-token="'.session()->value("csrf").'"';
 		$_ .= '	>';
 		$_ .= '	<h2 class="comments">Comments</h2>';
+		// Check if item has comments.
 		if($item["comments"]):
 			$_ .= '<ul class="comments">';
+			//  Loop through the comments and contaginate them in a string together with their corresponding information (published-at and author).
 			foreach($item["comments"] as $comment):
 			$_ .= '<li class="comment comment_id:'.$comment["id"].'" itemprop="comment" itemscope itemtype="https://schema.org/Comment">';
 				$_ .= '<ul class="info">';
@@ -42,11 +46,12 @@ class HTML extends HTMLCore {
 
 
 /** 
-* The function checks if item has prices.
-* If the item has an offer, the function prints the formatted price of the offer and the default-price. It also checks for a subscibtion-method and prints it.
-* If the item does not have an offer but instead has a default-price, the default-price is printed together with a subscribtion-method. 
-* If the item does not have either an offer or a default-price the function prints 'Free'. 
-* If the item has a description, it is printed. 
+* Generate a contaginated string with price and description of item
+* 
+* @param String $item
+* @param String $_url 
+* @param String $description optional
+* @return String
 */
 	function frontendOffer($item, $url, $description = false) {
 	
@@ -62,18 +67,20 @@ class HTML extends HTMLCore {
 			$_ .= '<ul class="offer" itemscope itemtype="http://schema.org/Offer">';
 				$_ .= '<li class="name" itemprop="name" content="'.$item["name"].'"></li>';
 				$_ .= '<li class="currency" itemprop="priceCurrency" content="'.$page->currency().'"></li>';
-
+				// Generate offer price, if relevant 
 				if($offer_key !== false) {
 					$_ .= '<li class="price default">'.formatPrice($item["prices"][$default_key]).(isset($item["subscription_method"]) && $item["subscription_method"] && $item["prices"][$default_key]["price"] ? ' / '.$item["subscription_method"]["name"] : '').'</li>';
 					$_ .= '<li class="price offer" itemprop="price" content="'.$item["prices"][$offer_key]["price"].'">'.formatPrice($item["prices"][$offer_key]).(isset($item["subscription_method"]) && $item["subscription_method"] && $item["prices"][$default_key]["price"] ? ' / '.$item["subscription_method"]["name"] : '').'</li>';
 				}
+				// Generate default price 
 				else if($item["prices"][$default_key]["price"]) {
 					$_ .= '<li class="price" itemprop="price" content="'.$item["prices"][$default_key]["price"].'">'.formatPrice($item["prices"][$default_key]).(isset($item["subscription_method"]) && $item["subscription_method"] && $item["prices"][$default_key]["price"] ? ' / '.$item["subscription_method"]["name"] : '').'</li>';
 				}
+				// No price, so item is free 
 				else {
 					$_ .= '<li class="price" itemprop="price" content="'.$item["prices"][$default_key]["price"].'">Free</li>';
 				}
-
+				// Does the item have a description?
 				$_ .= '<li class="url" itemprop="url" content="'.$url.'"></li>';
 				if($description) {
 					$_ .= '<li class="description" itemprop="description">'.$description.'</li>';
@@ -87,13 +94,14 @@ class HTML extends HTMLCore {
 	}
 
 /**
-* The function takes three parameters: item, url and an optional parameter with the two options 'media' and 'sharing'. 
-* The function prints a list with meta-data and article-info such as published-at, modified-at, author, main-entity, publisher-info (name, logo) and image. 
-* If the function gets media as a parameter, it prints meta-data of the media for SEO. If not, it prints meta-data of the logo for SEO. 
-* If the item has a location with longitude- and latitudedata it is printed. 
-
-* Note: Errors in publisher-info.  The function prints think.dk as name and uses logo-large instead of the correct KBHFF-logo. 
-* Note: Errors in meta-data of media. Wrong logo path as in publisher-info. 
+* 
+* Generate a contaginated string with meta-data and article-info suitable for SEO
+* 
+* @param String $item
+* @param String $url 
+* @param ARRAY $options 
+* $media BOOLEAN
+* $sharing BOOLEAN
 */ 
 	function articleInfo($item, $url, $_options) {
 
@@ -119,7 +127,7 @@ class HTML extends HTMLCore {
 		$_ .= '	<li class="main_entity'. ($sharing ? ' share' : '') .'" itemprop="mainEntityOfPage" content="'. SITE_URL.$url .'"></li>';
 		$_ .= '	<li class="publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">';
 		$_ .= '		<ul class="publisher_info">';
-		$_ .= '			<li class="name" itemprop="name">think.dk</li>';
+		$_ .= '			<li class="name" itemprop="name">Københavns Fødevarefællesskab</li>';
 		$_ .= '			<li class="logo" itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">';
 		$_ .= '				<span class="image_url" itemprop="url" content="'. SITE_URL .'/img/logo-large.png"></span>';
 		$_ .= '				<span class="image_width" itemprop="width" content="720"></span>';
@@ -128,7 +136,7 @@ class HTML extends HTMLCore {
 		$_ .= '		</ul>';
 		$_ .= '	</li>';
 		$_ .= '	<li class="image_info" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">';
-
+		
 		if($media):
 			$_ .= '		<span class="image_url" itemprop="url" content="'. SITE_URL .'/images/'. $item["item_id"] .'/'. $media["variant"] .'/720x.'. $media["format"] .'"></span>';
 			$_ .= '		<span class="image_width" itemprop="width" content="720"></span>';
@@ -157,21 +165,19 @@ class HTML extends HTMLCore {
 
 	}
 
-
-	// $context should be array of allowed contexts
-	// - if $context is false, no tags are shown (except editing and default tag)
-	// $default should be array with url and text
-	// $url should be url to prefix tag links
-	// $editing defines if editing link is shown
 	
 	/**
-	* The function takes two parameters: item and an optional with the options context, default, url, editing and schema.
-	* If the item has tags and an editing link is shown, the function checks if context and editing exist as a keyvalue pair and returns the index as an editing-tag.
-	* If the editing-tag is returned and it has a value of true, the function prints 'still editing'.
-	* If the editing-tag does not have a value of true, it prints the value.
-	* If a default-array is given as parameter, the function prints the url and the text.
-	* If the item has tags and context is defined, the function loops through the item-tags and creates a list of each item-tag containing its url and text and if relevant its schema-properties for SEO.
-	* The function prints a tag ul with the content. 
+	* Generate a contaginated string with a list of tags. 
+	* 
+	* @param String $item
+	* @param ARRAY $options 
+	* $context Array of allowed contexts - if $context is false, no tags are shown (except editing and default tag)
+	* $default Array with url and text
+	* $url String url to prefix tag links
+	* $editing BOOLEAN defines if editing link is shown
+	* $schema String
+	* @return STRING 
+	*
 	*/
 	function articleTags($item, $_options = false) {
 
@@ -202,7 +208,7 @@ class HTML extends HTMLCore {
 		$_ = '';
 
 
-		// editing tag
+		// Generate editing tag if relevant
 		if($item["tags"] && $editing):
 			$editing_tag = arrayKeyValue($item["tags"], "context", "editing");
 			if($editing_tag !== false):
@@ -210,12 +216,12 @@ class HTML extends HTMLCore {
 			endif;
 		endif;
 
-		// default tag
+		// Generate default tag if relevant
 		if(is_array($default)):
 			$_ .= '	<li><a href="'.$default[0].'">'.$default[1].'</a></li>';
 		endif;
 
-		// item tag list
+		// Generate item tag list
 		if($item["tags"] && $context):
 			foreach($item["tags"] as $item_tag):
 				if(array_search($item_tag["context"], $context) !== false):
@@ -244,8 +250,10 @@ class HTML extends HTMLCore {
 
 
 	/**
-	* The function checks if there are any messages, gets all stored messages and creates an array of these.
-	* It then loops through all the stored messages to get the type and message and then prints this information in a div. 
+	* Generate a contaginated string with all stored messages. 
+	*
+	* @param String $type message delivery type
+	* @return String messages
 	*/
 
 	function serverMessages($type = []) {
