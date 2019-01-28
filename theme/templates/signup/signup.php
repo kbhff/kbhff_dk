0e2ae5d3a9908de1af9fae07ff4d5b0749de48cc
ElisabethSH <e.haabegaard@gmail.com>
2019-01-28 10:51:18 +0100

    Updates

--- theme/templates/signup/signup.php
+++ theme/templates/signup/signup.php
@@ -73,6 +73,7 @@ if(message()->hasMessages(array("type" => "error"))): ?>
 		<?= $model->input("lastname", array("required" => true, "label" => "Efternavn", "hint_message" => "Skriv dit efternavn her", "error_message" => "Dit efternavn må kun indeholde bogstaver.")) ?>
 		<?= $model->input("email", array("required" => true, "label" => "Din email", "value" => $email, "hint_message" => "Indtast din email.", "error_message" => "Du har indtastet en ugyldig emailadresse.")); ?>
 		<?= $model->input("confirm_email", array("label" => "Gentag din email", "required" => true, "hint_message" => "Indtast din email igen.", "error_message" => "De to email adresser er ikke ens.")); ?>
+		<?= $model->input("mobile", array("label" => "Mobilnummer", "hint_message" => "Indtast dit mobilnummer.", "error_message" => "Det skal være et dansk mobilnummer.")); ?>
 		<?= $model->input("password", array("required" => true, "label" => "Adgangskode", "hint_message" => "Indtast en adgangskode.", "error_message" => "Din adgangskode skal være mellem 8 og 20 karakterer.")); ?>
 		<?= $model->input("department_id", array("type" => "select", "label" => "Vælg lokalafdeling", "options" => $HTML->toOptions($departments, "id", "name", ["add" => ["" => "Vælg afdeling"]]),)); ?>
 		<p class="terms_summary">Vi kan godt lide privatliv. Derfor indsamler vi kun de oplysninger vi skal bruge, og vi kontakter dig kun når vi vurderer at det er i din interesse.</p>
