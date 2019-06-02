ccae357cbf5e1548d1168ee6485afa11161b0643
Guybrush <36038081+Threepwoodd@users.noreply.github.com>
2019-06-02 19:47:24 +0200

    Squashed 'theme/www/assets/kbhff-skin/' changes from 511fdcc..171df3a
    171df3a Update manipulator bundle
    
    git-subtree-dir: theme/www/assets/kbhff-skin
    git-subtree-split: 171df3a9a1598f8d95ed7153a1ed2981c6b0dcf7

--- js/manipulator/merged/seg_desktop.js
+++ js/manipulator/merged/seg_desktop.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-js-merged @ 2019-04-09 18:32:10
+js-merged @ 2019-06-02 19:43:22
 */
 
 /*seg_desktop_include.js*/
@@ -256,18 +256,22 @@ Util.Animation = u.a = new function() {
 		}
 		node._x = x;
 		node._y = y;
+		node.offsetHeight;
 	}
 	this.rotate = function(node, deg) {
 		u.as(node, "transform", "rotate("+deg+"deg)");
 		node._rotation = deg;
+		node.offsetHeight;
 	}
 	this.scale = function(node, scale) {
 		u.as(node, "transform", "scale("+scale+")");
 		node._scale = scale;
+		node.offsetHeight;
 	}
 	this.setOpacity = this.opacity = function(node, opacity) {
 		u.as(node, "opacity", opacity);
 		node._opacity = opacity;
+		node.offsetHeight;
 	}
 	this.setWidth = this.width = function(node, width) {
 		width = width.toString().match(/\%|auto|px/) ? width : (width + "px");
@@ -862,15 +866,15 @@ Util.removeClass = u.rc = function(node, classname, dom_update) {
 }
 Util.toggleClass = u.tc = function(node, classname, _classname, dom_update) {
 	if(u.hc(node, classname)) {
-		u.rc(node, classname);
+		u.rc(node, classname, dom_update);
 		if(_classname) {
-			u.ac(node, _classname);
+			u.ac(node, _classname, dom_update);
 		}
 	}
 	else {
 		u.ac(node, classname);
 		if(_classname) {
-			u.rc(node, _classname);
+			u.rc(node, _classname, dom_update);
 		}
 	}
 	dom_update = (dom_update === false) || (node.offsetTop);
@@ -1260,6 +1264,7 @@ Util.Events = u.e = new function() {
 			}
 			if(this.e_drag || this.e_swipe) {
 				u.e.addMoveEvent(this, u.e._pick);
+				u.e.addEndEvent(this, u.e._cancelPick);
 			}
 			if(this.e_scroll) {
 				u.e.addMoveEvent(this, u.e._scrollStart);
@@ -1547,8 +1552,9 @@ u.e.resetDragEvents = function(node) {
 	this.removeEvent(node, "touchmove", this._drag);
 	this.removeEvent(node, "mouseup", this._drop);
 	this.removeEvent(node, "touchend", this._drop);
-	this.removeEvent(node, "mouseout", this._drop_out);
-	this.removeEvent(node, "mouseover", this._drop_over);
+	this.removeEvent(node, "mouseup", this._cancelPick);
+	this.removeEvent(node, "touchend", this._cancelPick);
+	this.removeEvent(node, "mouseout", this._dropOut);
 	this.removeEvent(node, "mousemove", this._scrollStart);
 	this.removeEvent(node, "touchmove", this._scrollStart);
 	this.removeEvent(node, "mousemove", this._scrolling);
@@ -1671,7 +1677,7 @@ u.e._pick = function(event) {
 				// 	
 				this._dropOutDrag = u.e._drag;
 				this._dropOutDrop = u.e._drop;
-				u.e.addOutEvent(this, u.e._drop_out);
+				u.e.addOutEvent(this, u.e._dropOut);
 			}
 		}
 	}
@@ -1687,6 +1693,8 @@ u.e._drag = function(event) {
 	}
 	this.current_xps = Math.round(((this.current_x - this.move_last_x) / (event.timeStamp - this.move_timestamp)) * 1000);
 	this.current_yps = Math.round(((this.current_y - this.move_last_y) / (event.timeStamp - this.move_timestamp)) * 1000);
+	this.last_x_distance_travelled = (this.current_xps) ? this.current_x - this.move_last_x : this.last_x_distance_travelled;
+	this.last_y_distance_travelled = (this.current_yps) ? this.current_y - this.move_last_y : this.last_y_distance_travelled;
 	this.move_timestamp = event.timeStamp;
 	this.move_last_x = this.current_x;
 	this.move_last_y = this.current_y;
@@ -1702,7 +1710,7 @@ u.e._drag = function(event) {
 	}
 	if(this.e_swipe) {
 		if(this.only_horizontal) {
-			if(this.current_xps < 0) {
+			if(this.current_xps < 0 || this.current_xps === 0 && this.last_x_distance_travelled < 0) {
 				this.swiped = "left";
 			}
 			else {
@@ -1710,7 +1718,7 @@ u.e._drag = function(event) {
 			}
 		}
 		else if(this.only_vertical) {
-			if(this.current_yps < 0) {
+			if(this.current_yps < 0 || this.current_yps === 0 && this.last_y_distance_travelled < 0) {
 				this.swiped = "up";
 			}
 			else {
@@ -1852,7 +1860,7 @@ u.e._drop = function(event) {
 		this[this.callback_dropped](event);
 	}
 }
-u.e._drop_out = function(event) {
+u.e._dropOut = function(event) {
 	this._drop_out_id = u.randomString();
 	document["_DroppedOutNode" + this._drop_out_id] = this;
 	eval('document["_DroppedOutMove' + this._drop_out_id + '"] = function(event) {document["_DroppedOutNode' + this._drop_out_id + '"]._dropOutDrag(event);}');
@@ -1861,6 +1869,12 @@ u.e._drop_out = function(event) {
 	u.e.addEvent(document, "mousemove", document["_DroppedOutMove" + this._drop_out_id]);
 	u.e.addEvent(this, "mouseover", document["_DroppedOutOver" + this._drop_out_id]);
 	u.e.addEvent(document, "mouseup", document["_DroppedOutEnd" + this._drop_out_id]);
+}
+u.e._cancelPick = function(event) {
+	u.e.resetDragEvents(this);
+	if(fun(this.pickCancelled)) {
+		this.pickCancelled(event);
+	}
 }
 u.e.setDragBoundaries = function(node, boundaries) {
 	if((boundaries.constructor && boundaries.constructor.toString().match("Array")) || (boundaries.scopeName && boundaries.scopeName != "HTML")) {
@@ -1897,7 +1911,7 @@ u.e.setDragBoundaries = function(node, boundaries) {
 		if(document.readyState && document.readyState == "interactive") {
 			debug_bounds.innerHTML = "WARNING - injected on DOMLoaded"; 
 		}
-		u.bug("node: "+u.nodeId(node)+" in (" + u.absX(node) + "," + u.absY(node) + "), (" + (u.absX(node)+node.offsetWidth) + "," + (u.absY(node)+node.offsetHeight) +")");
+		u.bug("node: ", node, " in (" + u.absX(node) + "," + u.absY(node) + "), (" + (u.absX(node)+node.offsetWidth) + "," + (u.absY(node)+node.offsetHeight) +")");
 		u.bug("boundaries: (" + node.start_drag_x + "," + node.start_drag_y + "), (" + node.end_drag_x + ", " + node.end_drag_y + ")");
 	}
 	node._x = node._x ? node._x : 0;
@@ -3915,9 +3929,6 @@ Util.request = function(node, url, _options) {
 		node[request_id].HTTPRequest = this.createRequestObject();
 		node[request_id].HTTPRequest.node = node;
 		node[request_id].HTTPRequest.request_id = request_id;
-		if(node[request_id].response_type) {
-			node[request_id].HTTPRequest.responseType = node[request_id].response_type;
-		}
 		if(node[request_id].request_async) {
 			node[request_id].HTTPRequest.statechanged = function() {
 				if(this.readyState == 4 || this.IEreadyState) {
@@ -3933,6 +3944,9 @@ Util.request = function(node, url, _options) {
 				var params = u.JSONtoParams(node[request_id].request_data);
 				node[request_id].request_url += params ? ((!node[request_id].request_url.match(/\?/g) ? "?" : "&") + params) : "";
 				node[request_id].HTTPRequest.open(node[request_id].request_method, node[request_id].request_url, node[request_id].request_async);
+				if(node[request_id].response_type) {
+					node[request_id].HTTPRequest.responseType = node[request_id].response_type;
+				}
 				if(node[request_id].request_timeout) {
 					node[request_id].HTTPRequest.timeout = node[request_id].request_timeout;
 				}
@@ -3959,6 +3973,9 @@ Util.request = function(node, url, _options) {
 					params = node[request_id].request_data;
 				}
 				node[request_id].HTTPRequest.open(node[request_id].request_method, node[request_id].request_url, node[request_id].request_async);
+				if(node[request_id].response_type) {
+					node[request_id].HTTPRequest.responseType = node[request_id].response_type;
+				}
 				if(node[request_id].request_timeout) {
 					node[request_id].HTTPRequest.timeout = node[request_id].request_timeout;
 				}
