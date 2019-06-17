474368ec3a8e0dab1b11354acd1be7171de7019f
Peter <36038081+Threepwoodd@users.noreply.github.com>
2019-06-17 16:54:07 +0200

    Translate to danish

--- theme/templates/posts/post.php
+++ theme/templates/posts/post.php
@@ -95,7 +95,7 @@ $related_items = $IC->getRelatedItems($related_pattern);
 
 <? if($related_items): ?>
 	<div class="related">
-		<h2>Related posts</h2>
+		<h2>Relaterede artikler</h2>
 
 		<ul class="items articles i:articleMiniList">
 <?		foreach($related_items as $item): 
