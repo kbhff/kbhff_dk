c1dfccdb6385140f620a5a8a2284898e77098dc2
Martin Kæstel Nielsen <martin@think.dk>
2019-06-21 16:37:05 +0200

    Custom post item

---
+++ theme/classes/items/type.post.class.php
@@ -0,0 +1,68 @@
+<?php
+/**
+* @package janitor.itemtypes
+* This file contains itemtype functionality
+*/
+
+class TypePost extends Itemtype {
+
+	/**
+	* Init, set varnames, validation rules
+	*/
+	function __construct() {
+
+		// construct ItemType before adding to model
+		parent::__construct(get_class());
+
+
+		// itemtype database
+		$this->db = SITE_DB.".item_post";
+
+
+		// Published
+		$this->addToModel("published_at", array(
+			"hint_message" => "Publishing date of the post. Leave empty for current time",
+		));
+
+		// Name
+		$this->addToModel("name", array(
+			"type" => "string",
+			"label" => "Name",
+			"required" => true,
+			"hint_message" => "Name your post", 
+			"error_message" => "Name must be filled out."
+		));
+
+		// Class
+		$this->addToModel("classname", array(
+			"type" => "string",
+			"label" => "CSS Class",
+			"hint_message" => "CSS class for custom styling. If you don't know what this is, just leave it empty."
+		));
+
+		// description
+		$this->addToModel("description", array(
+			"type" => "text",
+			"label" => "Short description",
+			"hint_message" => "Write a short description of the post",
+			"error_message" => "A short description without any words? How weird."
+		));
+
+		// HTML
+		$this->addToModel("html", array(
+			"hint_message" => "Write your the post",
+			"allowed_tags" => "p,h2,h3,h4,ul,ol,download,jpg,png,vimeo,youtube", //,mp4,code",
+		));
+
+		// Files
+		$this->addToModel("mediae", array(
+			"label" => "Add media here",
+			"allowed_formats" => "png,jpg,mp4",
+			"hint_message" => "Add images or videos here. Use png, jpg or mp4.",
+		));
+
+	}
+
+}
+
+?>
