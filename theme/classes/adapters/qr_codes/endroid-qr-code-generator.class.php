f20a4b9eee0cb2a20d1a87bbdde62c6cf62516d2
Søren Lyhne <lyhnesoren@gmail.com>
2020-02-12 09:33:09 +0100

    add qr code generator

---
+++ theme/classes/adapters/qr_codes/endroid-qr-code-generator.class.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once('includes/qr_codes/endroid-qr-code-3.7.5/vendor/autoload.php');
+
+use Endroid\QrCode\QrCode;
+
+class JanitorEndroidQrCodeGenerator {
+
+
+	/**
+	* Init, set varnames, validation rules
+	*/
+	function __construct() {}
+
+	function create($content, $output_file, $_options) {
+
+		$size = false;
+		$margin = false;
+		$foreground_color = false;
+		$background_color = false;
+		$format = false;
+
+		if($_options !== false) {
+			foreach($_options as $_option => $_value) {
+				switch($_option) {
+	
+					case "size"                   : $size                   = $_value; break;
+					case "margin"                 : $margin                 = $_value; break;
+					case "foreground_color"       : $foreground_color       = $_value; break;
+					case "background_color"       : $background_color       = $_value; break;
+					
+					case "format"                 : $format                 = $_value; break;
+	
+				}
+			}
+		}
+
+		if($content !== false) {
+
+			if(is_array($content)) {
+
+				$content_string = json_encode($content);
+			}
+
+			else {
+
+				$content_string = $content;
+			}
+
+			$QRC = new QrCode($content_string);
+	
+			if($size) {
+				$QRC->setSize($size);
+			}
+	
+			if($margin) {
+				$QRC->setMargin($margin);
+			}
+	
+			if($foreground_color) {
+				$QRC->setForegroundColor($foreground_color);
+			}
+	
+			if($background_color) {
+				$QRC->setBackgroundColor($background_color);
+			}
+	
+			if($format) {
+	
+				$QRC->setWriterByName($format);
+			}
+	
+			$QRC->writeFile($output_file);
+	
+			if(file_exists($output_file)) {
+				
+				return $output_file;
+			}
+		}
+
+		return false;
+
+	}
+
+}
---
+++ theme/classes/helpers/qr_codes.class.php
@@ -0,0 +1,76 @@
+<?php
+
+class QrCodesGateway {
+
+	private $adapter;
+
+	function __construct() {
+
+		// no adapter selected yet
+		$this->adapter = false;
+	}
+
+	function init_adapter() {
+
+		if(!$this->adapter) {
+
+			include_once("classes/adapters/qr_codes/endroid-qr-code-generator.class.php");
+			$this->adapter = new JanitorEndroidQrCodeGenerator();
+		}
+	}
+
+	/**
+	 * QrCodesGateway::create
+	 *
+	 * @param string|array|mixed $content – array is converted to json-encoded string. Everything else is directly converted to string. 
+	 * @param string $output_file – intended path of generated qr code
+	 * @param array|false $_options
+	 * * size (number): size in px
+	 * * margin (number): margin size in px
+	 * * foreground_color (array): rgba array, e.g. ["r" => 255, "g" => 255, "b" => 255, "a" => 0]
+	 * * background_color (array): rgba array
+	 * * format (string): png (default) or svg
+	 * 
+	 * @return string|false path of generated qr code (via adapter class). False on error.
+	 */
+	function create($content, $output_file, $_options = false) {
+
+		$this->init_adapter();
+
+		if($this->adapter) {
+
+			$size = false;
+			$margin = false;
+			$foreground_color = false;
+			$background_color = false;
+			$format = false;
+
+			if($_options !== false) {
+				foreach($_options as $_option => $_value) {
+					switch($_option) {
+		
+						case "size"                   : $size                   = $_value; break;
+						case "margin"                 : $margin                 = $_value; break;
+						case "foreground_color"       : $foreground_color       = $_value; break;
+						case "background_color"       : $background_color       = $_value; break;
+						
+						case "format"                 : $format                 = $_value; break;
+		
+					}
+				}
+			}
+
+			return $this->adapter->create($content, $output_file, [
+
+				"size" => $size,
+				"margin" => $margin,
+				"foreground_color" => $foreground_color,
+				"background_color" => $background_color,
+
+				"format" => $format,
+
+			]);
+
+		}
+	}
+}
