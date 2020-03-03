be1bc4e251984fa183ad3f5ebfd950d619d2a8b1
Søren Lyhne <lyhnesoren@gmail.com>
2020-03-03 14:18:50 +0100

    fix constraint

--- theme/config/db/department_products.sql
+++ theme/config/db/department_products.sql
@@ -6,6 +6,6 @@ CREATE TABLE `SITE_DB`.`department_products` (
   PRIMARY KEY (`id`),
   KEY `department_id` (`department_id`),
   KEY `product_id` (`product_id`),
-  CONSTRAINT `department_products_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `SITE_DB`.`system_departments` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `department_products_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `SITE_DB`.`system_departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `department_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `SITE_DB`.`item_product` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
