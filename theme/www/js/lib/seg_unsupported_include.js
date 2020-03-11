b513bba44ecdb610e99ae576657a1a101053483d
Peter Kai Møller Pedersen <36038081+Threepwoodd@users.noreply.github.com>
2020-03-11 15:38:54 +0100

    Update js includes

--- theme/www/js/lib/seg_unsupported_include.js
+++ theme/www/js/lib/seg_unsupported_include.js
@@ -1,1 +1,1 @@
-document.write('<script type="text/javascript" src="/js/lib/i-unsupported.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/m-unsupported.js"></script>');
