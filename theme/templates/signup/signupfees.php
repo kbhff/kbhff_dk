e78989e38955fdc07ac3303ed4152716ff9e1a80
Martin Kæstel Nielsen <martin@think.dk>
2020-03-24 19:19:07 +0100

    Update signupfees page

--- theme/templates/signup/signupfees.php
+++ theme/templates/signup/signupfees.php
@@ -88,13 +88,15 @@ $signupfees = $IC->getItems(array("itemtype" => "signupfee", "order" => "positio
 				<? } ?>
 					<li class="url" itemprop="url" content="<?$url?>"></li>
 			<? }
-						
+
 				if($signupfee["html"]) { ?>
 					<li class="description" itemprop="description"><?=$signupfee["html"]?></li>
-			<? } ?>
+				<? } ?>
 			
 				</ul>
 
+				<?
+				/*
 				<ul class="offer" itemscope itemtype="http://schema.org/Offer">
 					<li class="name" itemprop="name" content="<?= $membership_item["name"] ?>"></li>
 					<li class="currency" itemprop="priceCurrency" content="<?= $this->currency() ?>"></li>
@@ -117,16 +119,18 @@ $signupfees = $IC->getItems(array("itemtype" => "signupfee", "order" => "positio
 					<li class="url" itemprop="url" content="<?$url?>"></li>
 				<? } ?>
 				</ul>
+				*/
+				?>
 
-					<?= $model->formStart("/bliv-medlem/addToCart", array("class" => "signup labelstyle:inject")) ?>
+				<?= $model->formStart("/bliv-medlem/addToCart", array("class" => "signup labelstyle:inject")) ?>
 					<?= $model->input("quantity", array("value" => 1, "type" => "hidden", "id" => "input_quantity_$i")); ?>
 					<?= $model->input("item_id", array("value" => $signupfee["item_id"], "type" => "hidden", "id" => "input_item_id_$i")); ?>
 
-				<ul class="actions">
+					<ul class="actions">
 						<?= $model->link("Læs mere her", "/bliv-medlem/medlemskaber/".$membership_item["fixed_url_identifier"], array("wrapper" => "li.readmore")) ?>
-						<?= $model->submit("Tilmeld", array("class" => "primary", "wrapper" => "li.signup")) ?>
-				</ul>
-					<?= $model->formEnd() ?>
+						<?= $model->submit("Jeg vil være " . $membership_item["name"], array("class" => "primary", "wrapper" => "li.signup")) ?>
+					</ul>
+				<?= $model->formEnd() ?>
 
 			</li>
 	<? endforeach; ?>
