27b0aa09cfe076c5da45bcca55760fe7dab489be
Søren Lyhne <lyhnesoren@gmail.com>
2020-10-27 14:27:09 +0100

    update text

--- theme/templates/member-help/user_profile.php
+++ theme/templates/member-help/user_profile.php
@@ -128,7 +128,7 @@ $unpaid_orders = $SC->getUnpaidOrders(["user_id" => $user_id]);
 				<? endforeach; ?>	
 				<? else: ?>
 				<div>
-					<p>Du har ingen aktuelle grøntsagsbestillinger.</p>					
+					<p><?= $member_name ?> har ingen aktuelle grøntsagsbestillinger.</p>					
 				</div>
 				<? endif; ?>
 
