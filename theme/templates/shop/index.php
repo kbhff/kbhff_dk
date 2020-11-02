f1df9773fca395c9aad6ed8de5c1f091d012df22
Søren Lyhne <lyhnesoren@gmail.com>
2020-11-02 22:19:50 +0100

    typo

--- theme/templates/shop/index.php
+++ theme/templates/shop/index.php
@@ -182,7 +182,7 @@ else {
 									
 									<?= $HTML->oneButtonForm("+", "/butik/addToCart", [
 										"confirm-value" => false,
-										"wait-value" => "Vent ..."
+										"wait-value" => "Vent ...",
 										"inputs" => [
 											"item_id" => $product["id"],
 											"quantity" => 1,
