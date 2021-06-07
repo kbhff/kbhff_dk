ef2552b75644bb275cdc20939f523d9bcdff471a
kaestel <martin@think.dk>
2021-06-07 15:24:41 +0200

    Add structure to massmail page

--- theme/templates/mass-mail/index.php
+++ theme/templates/mass-mail/index.php
@@ -15,39 +15,49 @@ $send_to_all_members = $this->checkpermissions("/massemail", "/sendToAllMembers"
 
 
 ?>
-<div class="scene mass_mail i:scene" itemscope itemtype="http://schema.org/NewsArticle">
+<div class="scene massmail i:massmail">
 	<h1>Massemail</h1>
-	<? if($send_to_department_members && $send_to_all_members): ?>
-	<p>Her kan du udsende beskeder til KBHFF's medlemmer. Du kan vælge at sende en mail til alle medlemmer eller blot til medlemmerne af en specifik afdeling.</p>
-	<? else: ?>
-	<p>Her kan du udsende beskeder til medlemmerne af din afdeling (<?= $department["name"] ?>).</p>
-	<? endif; ?>
-	
-	<div class="item message i:defaultEdit i:collapseHeader">
-		<?= $model->formStart("sendKbhffMessage", array("class" => "labelstyle:inject")) ?>
+
+	<div class="c-wrapper">
+		<div class="c-one-half">
 
 			<? if($send_to_department_members && $send_to_all_members): ?>
-			<fieldset>
-				<h3>Vælg modtagere</h3>
-				<?= $model->input("department_id", ["options" => $model->toOptions($departments, "id", "name", ["add" => [
-					"all_departments" => "Alle afdelinger",
-					"all_departments_all_members" => "Alle afdelinger (inkl. inaktive medlemmer)"
-				]])]) ?>
-			</fieldset>
+			<p>Her kan du udsende beskeder til KBHFF's medlemmer. Du kan vælge at sende en mail til alle medlemmer eller blot til medlemmerne af en specifik afdeling.</p>
+			<? else: ?>
+			<p>Her kan du udsende beskeder til medlemmerne af din afdeling (<?= $department["name"] ?>).</p>
 			<? endif; ?>
 
-			<fieldset>
-				<h3>Indhold</h3>
-				<?= $model->input("name", ["label" => "Emne"]) ?>
-				<?= $model->input("description", ["class" => "autoexpand short", "label" => "Kort tekst til mail-preview"]) ?>
-				<?= $model->input("html", ["label" => "Indhold"]) ?>
-			</fieldset>
+		</div>
 
-			<ul class="actions">
-				<?= $model->submit("Send", array("class" => "primary", "wrapper" => "li.submit")) ?>
-			</ul>
+		<div class="c-one-half">
+	
+			<?= $model->formStart("sendKbhffMessage", array("class" => "labelstyle:inject")) ?>
 
-		<?= $model->formEnd() ?>
+				<? if($send_to_department_members && $send_to_all_members): ?>
+				<fieldset>
+					<h3>Vælg modtagere</h3>
+					<?= $model->input("department_id", ["options" => $model->toOptions($departments, "id", "name", ["add" => [
+						"all_departments" => "Alle afdelinger",
+						"all_departments_all_members" => "Alle afdelinger (inkl. inaktive medlemmer)"
+					]])]) ?>
+				</fieldset>
+				<? endif; ?>
+
+				<fieldset>
+					<h3>Indhold</h3>
+					<?= $model->input("name", ["label" => "Emne"]) ?>
+					<?= $model->input("description", ["class" => "autoexpand short", "label" => "Kort tekst til mail-preview"]) ?>
+					<?= $model->input("html", ["label" => "Indhold"]) ?>
+				</fieldset>
+
+				<ul class="actions">
+					<?= $model->submit("Send", array("class" => "primary", "wrapper" => "li.submit")) ?>
+				</ul>
+
+			<?= $model->formEnd() ?>
+
+		</div>
+
 	</div>
 
 
