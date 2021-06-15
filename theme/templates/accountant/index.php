52ea0ef6e0fcc902256ea4f4ec9476270c88f0ea
Josh Hunt <joshua.edward.hunt@gmail.com>
2021-06-15 20:43:37 +0200

    Add instructions for bogholder
    This include an explanation of how to use the bogholder page as well as a description of the issue with UTF-8 encoding in Excel and how to work around it.

--- theme/templates/accountant/index.php
+++ theme/templates/accountant/index.php
@@ -9,6 +9,21 @@ $departments = $DC->getDepartments();
 
 <div class="scene accountant departments i:scene" itemscope itemtype="http://schema.org/NewsArticle">
 	<h1>Bogholder</h1>
+
+	<p>Her kan du downloade data indtastet i butiksvagtens kasseregnskab: nederst kan du klikke dig
+igennem til en bestemt afdelings regnskab fra en bestemt dag. Alternativt, kan du øverst downloade
+en csv fil med alle kasseregnskab fra en enkelt dato.</p>
+
+	<p>NB. Hvis du skal åbne en csv fil med Excel og gerne vil kunne læse ‘æ’, ‘ø’ og ‘å’, skal du:
+		<ol>
+			<li>Åbne en ny Excel fil.</li>
+			<li>Klik på Data -> Fra tekst (i gruppen Hent eksterne data).</li>
+			<li>Vælg csv filen.</li>
+			<li>Vælg “afgrænset” og under Filoprindelse/Tegnsæt vælg “65001: Unicode (UTF-8)”.
+Klik på Næste.</li>
+			<li>Kryds af i Tabulering og Komma og klik Færdig.</li>
+		</ol>
+	</p>
 	
 	<div class="c-wrapper">
 		<h2>Download afregningsoversigt</h2>
