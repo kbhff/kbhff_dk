ecaf734b837cf08829e35b40ab87eb6309abd4d9
Søren Lyhne <lyhnesoren@gmail.com>
2021-09-03 16:30:26 +0200

    handle deliver buttons with AJAX

--- theme/www/js/lib/desktop/m-shop_shift.js
+++ theme/www/js/lib/desktop/m-shop_shift.js
@@ -19,7 +19,38 @@ Util.Modules["shop_shift"] = new function() {
 			form.updated = function() {
 				this.submit();
 			}
+
+			var confirm_delivery_listings = u.qsa(".orders.items .listing");
 			
+			var i, listing;
+			for(i = 0; i < confirm_delivery_listings.length; i++) {
+
+				listing = confirm_delivery_listings[i];
+				this.initDeliveryButton(listing);
+
+			}
+
+		}
+
+		scene.initDeliveryButton = function(listing) {
+
+			// u.bug(listing);
+			listing.order_item_id = u.cv(listing, "order_item_id");
+			listing.list = u.qs(".orders.items .list");
+			listing.scene = this;
+			listing.li = u.qs("li.confirm", listing);
+			listing.li.listing = listing;
+
+			listing.li.confirmed = function(response) {
+
+				response.listing = u.ge("order_item_id:" + listing.order_item_id, response);
+				response.listing.li = u.qs("li.confirm", response.listing);
+
+
+				this.listing.list.replaceChild(response.listing, this.listing);
+				u.m.oneButtonForm.init(response.listing.li);
+				this.listing.scene.initDeliveryButton(response.listing);
+			}
 		}
 
 		// scene is ready
