b2ffe5909c8d194b5c3c5c1e8d191b7c7f76913c
Søren Lyhne <lyhnesoren@gmail.com>
2022-04-06 12:54:01 +0200

    update member help order history

--- theme/templates/member-help/user_order_history.php
+++ theme/templates/member-help/user_order_history.php
@@ -18,6 +18,11 @@ $is_member = $member_user["membership"] ? $member_user["membership"]["id"] : fal
 
 $orders = $SC->getOrders(["user_id" => $member_user_id]);
 
+$orders = $SC->getOrders(["user_id" => $member_user_id]);
+$upcoming_order_items = $SC->getOrderItems(["user_id" => $member_user_id, "order" => "pp.pickupdate ASC, so.order_no", "department_pickupdate" => "only", "where" => "i.itemtype REGEXP '^product.*' AND pp.pickupdate >= CURDATE()"]);
+$past_order_items = $SC->getOrderItems(["user_id" => $member_user_id, "order" => "pp.pickupdate DESC, so.order_no", "department_pickupdate" => "only", "where" => "i.itemtype REGEXP '^product.*' AND pp.pickupdate < CURDATE()"]);
+$order_items_no_pickupdate = $SC->getOrderItems(["user_id" => $member_user_id, "order" => "so.order_no","department_pickupdate" => "none", "where" => "i.itemtype REGEXP '^product.*'"]);
+
 ?>
 
 
@@ -39,31 +44,196 @@ $orders = $SC->getOrders(["user_id" => $member_user_id]);
 		<? if($is_member): ?>
 		<div class="section orders">
 
-			<? if($orders): ?>
+			<? if($upcoming_order_items || $past_order_items || $order_items_no_pickupdate): ?>
 
-				<div class="orders">
-					<ul class="orders">
+				<? if($upcoming_order_items): ?>				
+				<div class="upcoming_order_items">
+					<h3>Kommende bestillinger</h3>
+					<ul class="order_items">
 
-					<? foreach($orders as $order):
-						$total_order_price = $SC->getTotalOrderPrice($order["id"]);
+					<? foreach($upcoming_order_items as $order_item):
+
+						
+						$order_item_log = $SC->getOrderItemLog($order_item["id"]);
+
 					 ?>
 
-						<li class="order">
-							<span class="order_date"><span class="date"><?= date("d.m.Y", strtotime($order["created_at"])) ?></span></span>
-							<span class="order_no"><?= $order["order_no"] ?></span>
-							<span class="total_price"><?= formatPrice($total_order_price) ?></span>
+						<li class="order_item">
+							<span class="pickupdate"><?= date("d.m.Y", strtotime($order_item["pickupdate"])) ?></span>
+							<span class="department">(<?= $order_item["department"] ?>):</span>
+							<span class="product"><?= $order_item["quantity"] > 1 ? $order_item["quantity"]." x " : ""?><?= $order_item["name"] ?></span> – 
+							<span class="order_no">Ordrenr. <?= $order_item["order_no"] ?> – </span>
+							<span class="order_date"><span class="date">Bestilt <?= date("d.m.Y", strtotime($order_item["created_at"])) ?></span></span>
+							<? if($order_item_log): ?>
+							<ul class="order_item_log_entries">
+							<? foreach($order_item_log AS $key => $entry): 
+								$log_user = $UC->getKbhffUser(["user_id" => $entry["user_id"]]);
+								?>
+								<? if($key !== 0): ?>
+								<? 
+								$log_user_name = $log_user["nickname"];
+								$log_user_member_no = isset($log_user["member_no"]) ? " (medlem nr. ".$log_user["member_no"].")" : false;
+								?>
+								<li class="order_item_log_entry">
+									<span class = "created_at"><?= date("d.m.Y H:i:s", strtotime($entry["created_at"])) ?></span>: 
+									<span class="log_user"><?= $log_user_name ?: "Du" ?></span> 
+									<? if($log_user_member_no && $log_user_name): ?>
+									<span class="log_user_member_no"><?= $log_user_member_no ?></span> 
+									<? endif; ?>
+									<? if($order_item_log[$key-1]["pickupdate"] && $entry["pickupdate"]): ?>
+									flyttede bestillingen fra
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span> 
+									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span> 
+									til 
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>.
+									<? elseif($order_item_log[$key-1]["pickupdate"]): ?>
+									fjernede bestillingen fra afhentningsdagen
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span> 
+									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span>
+									<? elseif($entry["pickupdate"]): ?>
+									planlagde afhentning af bestillingen:  
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>
+									<? endif; ?>
+								</li>
+								<? endif; ?>
+								<? endforeach; ?>
+							</ul>
+							<? endif; ?>
 						</li>
 
-						<? foreach($order["items"] as $order_item): ?>
-						<li class="order_item">
-							<span class="product"><?= $order_item["quantity"] > 1 ? $order_item["quantity"]." x " : ""?><?= $order_item["name"] ?></span>
-						</li>
-						<? endforeach; ?>
+
 
 					<? endforeach; ?>	
 
 					</ul>
 				</div>
+				<? endif; ?>
+
+				<? if($past_order_items): ?>				
+				<div class="past_order_items">
+					<h3>Tidligere bestillinger</h3>
+					<ul class="order_items">
+
+					<? foreach($past_order_items as $order_item):
+
+						
+						$order_item_log = $SC->getOrderItemLog($order_item["id"]);
+
+					 ?>
+
+						<li class="order_item">
+							<span class="pickupdate"><?= date("d.m.Y", strtotime($order_item["pickupdate"])) ?></span>
+							<span class="department">(<?= $order_item["department"] ?>):</span>
+							<span class="product"><?= $order_item["quantity"] > 1 ? $order_item["quantity"]." x " : ""?><?= $order_item["name"] ?></span> – 
+							<span class="order_no">Ordrenr. <?= $order_item["order_no"] ?> – </span>
+							<span class="order_date"><span class="date">Bestilt <?= date("d.m.Y", strtotime($order_item["created_at"])) ?></span></span>
+							<span class="delivery_status"><?= $order_item["shipped_by"] ? "– Leveret" : "– Ikke leveret" ?></span>
+							<? if($order_item_log): ?>
+							<ul class="order_item_log_entries">
+							<? foreach($order_item_log AS $key => $entry): 
+								$log_user = $UC->getKbhffUser(["user_id" => $entry["user_id"]]);
+								?>
+								<? if($key !== 0): ?>
+								<? 
+								$log_user_name = $log_user["nickname"];
+								$log_user_member_no = isset($log_user["member_no"]) ? " (medlem nr. ".$log_user["member_no"].")" : false;
+								?>
+								<li class="order_item_log_entry">
+									<span class = "created_at"><?= date("d.m.Y H:i:s", strtotime($entry["created_at"])) ?></span>: 
+									<span class="log_user"><?= $log_user_name ?: "Du" ?></span> 
+									<? if($log_user_member_no && $log_user_name): ?>
+									<span class="log_user_member_no"><?= $log_user_member_no ?></span> 
+									<? endif; ?>
+									<? if($order_item_log[$key-1]["pickupdate"] && $entry["pickupdate"]): ?>
+									flyttede bestillingen fra 
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span> 
+									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span> 
+									til 
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>.
+									<? elseif($order_item_log[$key-1]["pickupdate"]): ?>
+									fjernede bestillingen fra afhentningsdagen
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span>									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span>
+									<? elseif($entry["pickupdate"]): ?>
+									planlagde afhentning af bestillingen:  
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>
+									<? endif; ?>
+								</li>
+								<? endif; ?>
+								<? endforeach; ?>
+							</ul>
+							<? endif; ?>
+						</li>
+
+
+
+					<? endforeach; ?>	
+
+					</ul>
+				</div>
+				<? endif; ?>
+
+				<? if($order_items_no_pickupdate): ?>				
+				<div class="order_items_no_pickupdate">
+					<h3>Bestillinger uden afhentningstid og -sted</h3>
+					<p>Skriv til <a href="mailto:it@kbhff.dk">it@kbhff.dk</a> for at føje tid/sted til disse bestillinger.</p>
+					<ul class="order_items">
+
+					<? foreach($order_items_no_pickupdate as $order_item):
+
+						
+						$order_item_log = $SC->getOrderItemLog($order_item["id"]);
+
+					 ?>
+
+						<li class="order_item">
+							<span class="product"><?= $order_item["quantity"] > 1 ? $order_item["quantity"]." x " : ""?><?= $order_item["name"] ?></span> – 
+							<span class="order_no">Ordrenr. <?= $order_item["order_no"] ?> – </span>
+							<span class="order_date"><span class="date">Bestilt <?= date("d.m.Y", strtotime($order_item["created_at"])) ?></span></span>							<? if($order_item_log): ?>
+							<ul class="order_item_log_entries">
+								<? foreach($order_item_log AS $key => $entry): 
+								$log_user = $UC->getKbhffUser(["user_id" => $entry["user_id"]]);
+								?>
+								<? if($key !== 0): ?>
+								<? 
+								$log_user_name = $log_user["nickname"];
+								$log_user_member_no = isset($log_user["member_no"]) ? " (medlem nr. ".$log_user["member_no"].")" : false;
+								?>
+								<li class="order_item_log_entry">
+									<span class = "created_at"><?= date("d.m.Y H:i:s", strtotime($entry["created_at"])) ?></span>: 
+									<span class="log_user"><?= $log_user_name ?: "Du" ?></span> 
+									<? if($log_user_member_no && $log_user_name): ?>
+									<span class="log_user_member_no"><?= $log_user_member_no ?></span> 
+									<? endif; ?>
+									<? if($order_item_log[$key-1]["pickupdate"] && $entry["pickupdate"]): ?>
+									flyttede bestillingen fra 
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span>									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span> 
+									til 
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>.
+									<? elseif($order_item_log[$key-1]["pickupdate"]): ?>
+									fjernede bestillingen fra afhentningsdagen
+									<span class="old_pickupdate"><?= date("d.m.Y", strtotime($order_item_log[$key-1]["pickupdate"])) ?></span>									<span class="old_department">(<?= $order_item_log[$key-1]["department"] ?>)</span>
+									<? elseif($entry["pickupdate"]): ?>
+									planlagde afhentning af bestillingen:  
+									<span class="new_pickupdate"><?= date("d.m.Y", strtotime($entry["pickupdate"])) ?></span> 
+									<span class="new_department">(<?= $entry["department"] ?>)</span>
+									<? endif; ?>
+								</li>
+								<? endif; ?>
+								<? endforeach; ?>
+							</ul>
+							<? endif; ?>
+						</li>
+
+					<? endforeach; ?>	
+
+					</ul>
+				</div>
+				<? endif; ?>
 
 			<? else: ?>
 			<p><?= $member_user_name ?> har ingen aktuelle grøntsagsbestillinger.</p>
