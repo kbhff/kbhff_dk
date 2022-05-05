2bcfe433573216872b3ea475009d45b8110c8020
kaestel <martin@think.dk>
2022-05-05 11:00:44 +0200

    Also update text in order_item

--- theme/www/fix.php
+++ theme/www/fix.php
@@ -13,6 +13,8 @@ $query = new Query();
 include_once("classes/shop/supersubscription.class.php");
 $SuperSubscriptionClass = new SuperSubscription();
 
+$ShopClass = new Shop();
+
 
 $sql = "SELECT * FROM ".$SuperSubscriptionClass->db_subscriptions." WHERE expires_at IS NULL";
 if($query->sql($sql)) {
@@ -24,6 +26,26 @@ if($query->sql($sql)) {
 		// debug([$user_subscription, $new_expiry]);
 
 		$sql = "UPDATE ".$SuperSubscriptionClass->db_subscriptions." SET expires_at = '$new_expiry' WHERE id = ".$user_subscription["id"];
+		$query->sql($sql);
+
+		// debug([$sql]);
+
+	}
+
+}
+
+$sql = "SELECT * FROM ".$ShopClass->db_order_items." WHERE name LIKE '%01/05/2022 - 01/01/1970%'";
+if($query->sql($sql)) {
+
+	$order_items = $query->results();
+	foreach($order_items as $order_item) {
+
+		// debug([$order_item]);
+
+		$updated_order_item_name = preg_replace("/automatic renewal/", "automatisk fornyelse", $order_item["name"]);
+		$updated_order_item_name = preg_replace("/01\/01\/1970/", "01/05/2023", $updated_order_item_name);
+
+		$sql = "UPDATE ".$ShopClass->db_order_items." SET name = '$updated_order_item_name' WHERE id = ".$order_item["id"];
 		$query->sql($sql);
 
 		// debug([$sql]);
