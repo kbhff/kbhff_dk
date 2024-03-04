ceedff6d212b939b9555e8144efad038691e6ba6
Mathieu <mathieu.bulliot@gmail.com>
2024-03-04 19:05:36 +0100

    Change segment to smartphone.

--- theme/config/segments.php
+++ theme/config/segments.php
@@ -9,7 +9,7 @@ $segments_config["www"] = array(
 	"desktop_ie11"  => "desktop",
 	"desktop_ie10"  => "desktop",
 
-	"smartphone"    => "desktop",
+	"smartphone"    => "smartphone",
 
 	"desktop_ie9"   => "unsupported",
 	"desktop_light" => "unsupported",
