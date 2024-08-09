4305553fe203097663be310bd34e5993f0f47527
kaestel <martin@think.dk>
2024-08-09 18:04:43 +0200

    Fix cmd error

--- theme/www/janitor/cronjob.php
+++ theme/www/janitor/cronjob.php
@@ -157,7 +157,7 @@ if(is_array($action) && count($action)) {
 		$UC = new SuperUser();
 
 		$output = new Output();
-		$output->screen($UC->sendCompleteSignupReminder($action));
+		$output->screen($UC->deleteIncompleteSignups($action));
 		exit();
 
 	}
